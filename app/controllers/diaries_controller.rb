fb241ce04fdfaf029caa8fc203d81ecf1f9a5726
Nannaan <nonodera65@gmail.com>
2023-06-27 18:10:26 +0900

    新規投稿機能の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,4 +2,23 @@ class DiariesController < ApplicationController
   def index
     @diaries = Diary.all
   end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def diary_params
+    params.require(:diary).permit(:datetime, :content, :image, :emotion, :score)
+  end
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,3 @@
 class Diary < ApplicationRecord
+  validates :content, presence: true
 end
